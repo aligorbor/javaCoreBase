cbc7e36f04c8a81cf54ff9b1b40bf81c2192c9af
aligorbor <aligorbor@list.ru>
2020-12-25 13:53:53 +0300

    Поместил в Git задание 2 продвинутый уровень

---
+++ javaCoreAdvanced/lesson2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.javaCoreAdvanced.homeWork.lesson2;
+
+public class MyArrayDataException extends NumberFormatException {
+    public MyArrayDataException(String msg) {
+        super(msg);
+    }
+}
